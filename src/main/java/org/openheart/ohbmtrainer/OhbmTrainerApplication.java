fad78e634cab4d8909ebd0a08890f2d7a56e8bf5
Jonathan O'Donovan <jonathanodonovan@Jonathans-MacBook-Pro.local>
2019-05-11 14:27:19 +0100

    Basic boot app

---
+++ src/main/java/org/openheart/ohbmtrainer/OhbmTrainerApplication.java
@@ -0,0 +1,13 @@
+package org.openheart.ohbmtrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhbmTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhbmTrainerApplication.class, args);
+	}
+
+}
